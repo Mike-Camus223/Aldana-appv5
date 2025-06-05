2dd857b694a21a24578098c74121295b888fbe90
mike camps <michaelcamposvil@gmail.com>
2025-06-05 11:50:24 -0300

    test

--- src/app/features/landing/pages/novias/novias.component.ts
+++ src/app/features/landing/pages/novias/novias.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { NoviasTemplateComponent } from "../../../../shared/components/novias-template/novias-template.component";
+import { BreadcrumbComponent } from "../../../../shared/components/breadcrump/breadcrump.component";
+import { RouterModule } from '@angular/router';
+import { AppMenuItem } from '../../../../shared/utils/models/app-menu-item.model';
 
 @Component({
   selector: 'app-novias',
-  imports: [],
+  imports: [NoviasTemplateComponent, BreadcrumbComponent, RouterModule],
   templateUrl: './novias.component.html',
   styleUrl: './novias.component.css'
 })
 export class NoviasComponent {
+  breadcrumbItemsNovias: AppMenuItem[] = [
+    { icon: 'pi pi-home', route: '/home' },
+    { label: 'Novias', route: '/novias' }
+  ];
+
 
 }
