268df3ce9a4ae15a4954e5d06792627cb0e06b31
mike camps <michaelcamposvil@gmail.com>
2025-11-24 17:37:41 -0300

    modal generico agregado

--- src/app/shared/components/generic/modalv2/modalv2.component.ts
+++ src/app/shared/components/generic/modalv2/modalv2.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-modalv2',
-  imports: [],
-  templateUrl: './modalv2.component.html',
-  styleUrl: './modalv2.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modalv2.component.html'
 })
-export class Modalv2Component {
+export class Modalv2Component implements OnChanges {
+  @Input() open = false;
+  @Output() openChange = new EventEmitter<boolean>();
 
+  animate = false;
+
+  ngOnChanges() {
+    if (this.open) {
+      setTimeout(() => (this.animate = true), 10);
+    } else {
+      this.animate = false;
+    }
+  }
+
+  close() {
+    this.animate = false;
+    setTimeout(() => {
+      this.open = false;
+      this.openChange.emit(false);
+    }, 250);
+  }
 }
