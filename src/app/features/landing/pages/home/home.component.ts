9a598b8456a18ba8674e06020fe255858552b1be
michael <michaelcamposvil@gmail.com>
2026-05-25 10:27:00 -0300

    saved all

--- src/app/features/landing/pages/home/home.component.ts
+++ src/app/features/landing/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { AboutSecHomeComponent } from '../../../../shared/components/sections/ab
 import { ReelsSectionComponent } from "../../../../shared/components/sections/reels-section/reels-section.component";
 import { DualSectionComponent } from "../../../../shared/components/generic/dual-section/dual-section.component";
 import { WordRevealDirective } from '../../../../shared/utils/directives/word-reveal.directive';
+import { AboutSecHomev2Component } from '../../../../shared/components/sections/about-sec-homev2/about-sec-homev2.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, WordRevealDirective, AboutSecHomeComponent, ReelsSectionComponent, DualSectionComponent],
+  imports: [RouterModule, WordRevealDirective, AboutSecHomeComponent, ReelsSectionComponent, DualSectionComponent,AboutSecHomev2Component],
   templateUrl: './home.component.html',
   styles: ``
 })
