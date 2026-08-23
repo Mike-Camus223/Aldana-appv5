7a7b296b232b980137c1e88fd548a03cc6d17f54
mikecat <michaecamposvil@gmail.com>
2026-08-23 01:21:07 -0300

    tailored titles

--- src/app/features/landing/pages/tailored/tailored.component.ts
+++ src/app/features/landing/pages/tailored/tailored.component.ts
@@ -1,14 +1,26 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { WordRevealDirective } from '../../../../shared/utils/directives/word-reveal.directive';
+import { BreadcrumbComponent } from '../../../../shared/components/system/breadcrump/breadcrump.component';
+import { AppMenuItem } from '../../../../shared/utils/models/app-menu-item.model';
 
 @Component({
   selector: 'app-tailored',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    WordRevealDirective,
+    BreadcrumbComponent
+  ],
   templateUrl: './tailored.component.html',
   styleUrls: ['./tailored.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TailoredComponent {
+  breadcrumbItems: AppMenuItem[] = [
+    { label: 'INICIO', route: '/' },
+    { label: 'A MEDIDA', route: '/a-medida' }
+  ];
 }
